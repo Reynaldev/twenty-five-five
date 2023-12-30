725d695b44031f5754b4b9c94897f79a07f24bff
ReynaldyNC <reynaldy.nur.cahyo.003@gmail.com>
2023-12-30 21:36:26 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleUp, faAngleDown, faPlay, faRotateLeft } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const d = new Date();
+
+const timerLabel = {
+  Session: "Session",
+  Break: "Break"
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      breakTime: 5,
+      sessionTime: 25,
+      timerLabel: timerLabel.Session,
+      timeProgress: false,
+      timer: "00:00"
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        <div class="display">
+            <h5 id="timer-label">{this.state.timerLabel}</h5>
+            <h3 id="time-left">{this.state.timer}</h3>
+        </div>
+        <div class="row">
+          <div class="col">
+            <h4 id="break-label">Break Length</h4>
+            
+            <button id="break-increment"><FontAwesomeIcon icon={faAngleUp} /></button>
+            <h5 id="break-length" style={{padding: "8px"}}>{this.state.breakTime}</h5>
+            <button id="break-decrement"><FontAwesomeIcon icon={faAngleDown} /></button>
+          </div>
+          <div class="col-sm-2" style={{paddingTop: "4em", paddingBottom: "4em"}}>
+            <button id="start_stop"><FontAwesomeIcon icon={faPlay} /></button>
+            <button id="reset"><FontAwesomeIcon icon={faRotateLeft} /></button>
+          </div>
+          <div class="col">
+            <h4 id="session-label">Session Length</h4>
+
+            <button id="session-increment"><FontAwesomeIcon icon={faAngleUp} /></button>
+            <h5 id="session-length" style={{padding: "8px"}}>{this.state.sessionTime}</h5>
+            <button id="session-decrement"><FontAwesomeIcon icon={faAngleDown} /></button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
