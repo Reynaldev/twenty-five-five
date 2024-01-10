e9ad3864e660a7899a43bd210922b896405a0e48
ReynaldyNC <reynaldy.nur.cahyo.003@gmail.com>
2024-01-10 09:21:37 +0700

    Major update
    New
    - Session timer countdown
    - Reset timer
    - Increase/Decrease session length
    - Added timer offset, which will start when the timer is paused

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ const timerLabel = {
   Break: "Break"
 };
 
-let d = new Date();
-let timeout;
+let s = new Date();
+let timeout, offsetIntv;
+let currentMs = 0;
+let offsetTime = 0;
 
 class App extends React.Component {
   constructor(props) {
@@ -20,42 +22,108 @@ class App extends React.Component {
       sessionTime: 25,
       timerLabel: timerLabel.Session,
       timeProgress: false,
+      newTime: true,
       timer: "25:00"
     };
   
+    this.stopTime = this.stopTime.bind(this);
     this.startTime = this.startTime.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
+    this.clearOffset = this.clearOffset.bind(this);
     this.fmtTimeToStr = this.fmtTimeToStr.bind(this);
     this.setSessionTime = this.setSessionTime.bind(this);
+    this.startOffsetCount = this.startOffsetCount.bind(this);
   }
   startTime() {
     this.setState({
       timeProgress: (this.state.timeProgress) ? false : true
     }, () => {
-      let t = new Date(), s = new Date();
+      let t = new Date();
 
       if (this.state.timeProgress) {
         console.log("Pomodoro start!");
 
-        s.setTime(s.getTime() + (this.state.sessionTime * 60000));
+        if (this.state.newTime) {
+          s = new Date();
+          s.setTime(s.getTime() + (this.state.sessionTime * 60000));
+
+          console.log(`Ends at ${s.toTimeString()}`);
+
+          this.setState({
+            newTime: false
+          });
+        } else {
+          console.log(`Timer + offset: ${(new Date(s.getTime() + (offsetTime * 1000))).toTimeString()}`);
+          
+          s.setTime(s.getTime() + (offsetTime * 1000));
+
+          this.clearOffset();
+        }
 
         timeout = setInterval(() => {
-            t = new Date();
-            var currentMs = s.getTime() - t.getTime();
-            t.setTime(currentMs);
+          currentMs = s.getTime() - t.getTime();
+          t.setTime(currentMs);
 
-            this.fmtTimeToStr(t.getMinutes(), t.getSeconds());
+          // console.log(`Current date: ${t.toLocaleString()}`);
+          // console.log(`Time left (ms): ${currentMs}`);
+          
+          this.fmtTimeToStr(t.getMinutes(), t.getSeconds());
+
+          t = new Date();
+
+          if (currentMs <= 0) {
+            this.stopTime();
+
+            this.setState({
+              timeProgress: false,
+              newTime: true
+            });
+          }
         }, 500);
-      } else {      
-        console.log("Pomodoro stop!");
-        window.clearInterval(timeout);
+      } else {   
+        console.log(`Pause date: ${t.toTimeString()}`);
 
-        this.setState({
-          sessionTime: t.getMinutes()
-        });
+        this.stopTime();
+        this.startOffsetCount();
       }
     });
   }
+  stopTime() {   
+    window.clearInterval(timeout);
+    
+    // console.log(Math.floor((currentMs % (1000 * 60 * 60)) / (1000 * 60)));
+    console.log("Pomodoro stop!");
+  }
+  startOffsetCount() {
+    console.log("Counting offset");
+
+    offsetIntv = setInterval(() => {
+      offsetTime++;
+
+      console.log(`Offset (s): ${offsetTime}`);
+    }, 1000);
+  }
+  clearOffset() {
+    offsetTime = 0;
+    window.clearInterval(offsetIntv);
+  }
+  resetTimer() {
+    this.setState({
+      timerLabel: timerLabel.Session,
+      timeProgress: false,
+      newTime: true
+    }, () => {
+      currentMs = 0;
+
+      window.clearInterval(timeout);
+
+      this.clearOffset();
+      this.fmtTimeToStr(this.state.sessionTime);
+    });
+  }
   setSessionTime() {
+    if (this.state.timeProgress) return;
+
     var el = document.activeElement;
 
     switch (el.id) {
@@ -72,6 +140,10 @@ class App extends React.Component {
       default:
         break;
     }
+    
+    this.setState({
+      newTime: true
+    });
   }
   fmtTimeToStr(m, s = "00") {
     let minute = m.toString();
@@ -103,7 +175,7 @@ class App extends React.Component {
           </div>
           <div class="col-sm-2" style={{paddingTop: "4em", paddingBottom: "4em"}}>
             <button id="start_stop" onClick={this.startTime}><FontAwesomeIcon icon={faPlay} /></button>
-            <button id="reset"><FontAwesomeIcon icon={faRotateLeft} /></button>
+            <button id="reset" onClick={this.resetTimer}><FontAwesomeIcon icon={faRotateLeft} /></button>
           </div>
           <div class="col">
             <h4 id="session-label">Session Length</h4>
